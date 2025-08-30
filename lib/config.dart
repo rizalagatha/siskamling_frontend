8fa3c118588bd09b5d8dcfeff3a2cb94cc2be080
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-08-30 11:33:34 +0700

    Update Config

--- lib/config.dart
+++ lib/config.dart
@@ -3,6 +3,6 @@
 class Config {
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: 'http://192.168.1.191:3000/api',
+    defaultValue: 'http://192.168.1.73:4040/api',
   );
 }
